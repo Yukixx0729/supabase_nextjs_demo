f7f789b8f2a79e0784e546203903edd13b4b08b5
曾玉彬 <yuki@192-168-1-106.tpgi.com.au>
2023-07-31 23:07:57 +1000

    fix the bug on deployment

--- src/core/utils/index.js
+++ src/core/utils/index.js
@@ -1,3 +1,5 @@
 const production = process.env.NODE_ENV === "production";
 
-export const SITE_URL = production ? "prod-url" : "http://localhost:3000";
+export const SITE_URL = production
+  ? "https://supabase-nextjs-demo-ir3jks82h-yukixx0729.vercel.app"
+  : "http://localhost:3000";
