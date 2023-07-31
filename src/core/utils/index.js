6645b9e7c8c82c76ece8542c3763c38aa96322b1
曾玉彬 <yuki@192-168-1-106.tpgi.com.au>
2023-08-01 00:03:57 +1000

    change url

--- src/core/utils/index.js
+++ src/core/utils/index.js
@@ -1,5 +1,5 @@
 const production = process.env.NODE_ENV === "production";
 
 export const SITE_URL = production
-  ? "https://supabase-nextjs-demo-ir3jks82h-yukixx0729.vercel.app"
+  ? "https://supabase-nextjs-demo-aoqg.vercel.app/"
   : "http://localhost:3000";
